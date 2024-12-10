9e3d39425cecd9071db78df19dcadaaabf3df889
CeciFerrer <g.ceciliaferrer@gmail.com>
2024-12-10 15:12:59 +0100

    Limpiar componente con OnDestry()

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,7 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy{
+  currentStatus: 'online'|'offline'|'unknown' = 'offline';
+  private interval?: ReturnType<typeof setInterval>;
+  private interval?: NodeJS.Timeout;
+
+  constructor(){}  // mantener el constructor tan limpio como sea posible
   
+  ngOnInit(){
+    console.log('ON INIT');
+    this.interval = setInterval(()=>{
+      const rnd = Math.random();
+      if (rnd < 0.5){
+        this.currentStatus='online';
+      }else if (rnd < 0.9){
+        this.currentStatus='online';
+      }else {
+        this.currentStatus='unknown';
+      }
+    }, 5000);
+  }
+  ngAfterViewInit(){
+    console.log('AFTER VIEW INIT');
+  }
+  
+  ngOnDestroy(): void {
+    clearTimeout(this.interval);
+  }
 }
