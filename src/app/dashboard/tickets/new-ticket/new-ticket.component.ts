bd3a94ee59f35ca2340415ce983460255595d202
CeciFerrer <g.ceciliaferrer@gmail.com>
2024-12-10 19:15:52 +0100

    ultimo commit de este proyecto

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -14,6 +14,8 @@ export class NewTicketComponent implements OnInit, AfterViewInit{
   @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   //@Output() add= new EventEmitter<{title:string; text:string}>();
+  enteredTitle = '';
+  enteredText= '';
   add = output<{title:string; text:string}>();
 
   constructor(){}
@@ -28,9 +30,11 @@ export class NewTicketComponent implements OnInit, AfterViewInit{
     console.log(this.form?.nativeElement);
   }
 
-  onSubmit(title:string, ticketText: string){
-    this.add.emit({title: title, text: ticketText});
-    this.form?.nativeElement.reset();
+  onSubmit(){
+    this.add.emit({title: this.enteredTitle, text: this.enteredText});
+    //this.form?.nativeElement.reset();
+    this.enteredTitle= '';
+    this.enteredText= '';
   }
  
 }
